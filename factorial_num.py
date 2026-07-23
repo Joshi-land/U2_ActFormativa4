090ff15ea7d8217292ac2c0c02df80f94bed2d31
Joshu <aren.daven1907@gmail.com>
2026-07-23 00:08:51 -0600

    Programa:Factorial de N

---
+++ factorial_num.py
@@ -0,0 +1,17 @@
+print(""" 
+<<< Factorial de un Número >>> 
+
+- Dado un número calcular su factorial
+
+""" )
+
+num = int(input("Ingrese un número: \n"))
+factorial= 1 
+
+if num < 0:
+    print ("\n #ERROR FACTORIAL NO DETERMINADO PARA NUM NEGATIVOS")
+else:
+    for i in range(1, num +1):
+        factorial *= i
+print ("\n El factorial de ", num, " es ", factorial)
+    
