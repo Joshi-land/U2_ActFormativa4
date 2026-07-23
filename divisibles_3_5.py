e55ca604aea8b72fbdece72ab8c366ad07af7c1e
Joshu <aren.daven1907@gmail.com>
2026-07-22 23:48:03 -0600

    Programa:Multiplos de 3 y 5

---
+++ divisibles_3_5.py
@@ -0,0 +1,11 @@
+print ("""
+--- MÚLTIPLOS DE 3 Y 5 ---
+
+> Se mostraran los mútiplos comunes de 3 y 5 hasta 100
+
+""")
+for i in range(1,101):
+    if (i % 3 == 0) & (i % 5 == 0 ):
+        print ("\n",i,"\n")
+print()
+print("<<< PROGRAMA EJECUTADO CON EXITO >>>")
