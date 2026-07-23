dc6208ce4aaddc762341fcf66511653193f2cec0
Joshu <aren.daven1907@gmail.com>
2026-07-22 20:33:32 -0600

    Programa: Adivinar - Número

---
+++ adivinar_numero.py
@@ -0,0 +1,25 @@
+import random
+
+intentos = 0
+num_secreto = random.randint(1,100)
+print("""
+<<<<<<< ADIVINE EL NUMERO>>>>>>>
+""")
+while True:
+    num_intento = int(input("Ingrese su intento: "))
+    print (num_secreto)
+
+    if num_intento > num_secreto:
+        print("Demasiado alto, intente otra vez")
+        intentos += 1
+    elif num_intento < num_secreto:
+        print("Demasiado bajo, intente otra vez")
+        intentos += 1
+    else:
+        intentos +=1
+        print("¡¡BIEN HECHO HA ADIVINADO CORRECTAMENTE !!")
+        break
+
+print ("<<<<<<< JUEGO TERMINADO >>>>>>>")
+print ("Número: ", num_secreto)
+print ("Número de intentos: ", intentos)
